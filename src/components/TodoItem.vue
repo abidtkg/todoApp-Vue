da781d7347044ef4cdedde46e1a00191e3dbca34
Abid Hasan <abidtkg@gmail.com>
2020-06-20 00:36:32 +0600

    developing basics

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h3>{{ todo.id }} . {{ todo.title }} </h3>
+    </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    name: 'TodoItem',
+    props: ['todo']
+})
+</script>
+<style scoped>
+
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div v-for="todo in todos" v-bind:key="todo.id">
+            <TodoItem v-bind:todo="todo" />
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+import TodoItem from './TodoItem.vue';
+export default Vue.extend({
+    name: 'Todos',
+    components: {
+        TodoItem
+    },
+    props: ['todos']
+})
+</script>
+<style scoped>
+
+</style>
